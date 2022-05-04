177f54f86a7160932d71aaf3754094f5873d482f
Nortrix0 <bdgarth1997@gmail.com>
2022-05-04 08:09:51 -0500

    Added pacstrap command

--- kde-install.sh
+++ kde-install.sh
@@ -1,35 +1,4 @@
-#xorg
-#kwin
-#sddm
-#konsole
-#dolphin
-#kinfocenter
-#kate
-#firefox
-#kget
-#kmail
-#vlc
-#kdeconnect
-#kdiff3
-#krename
-#kfind
-#filelight
-#htop
-#kjots
-#kontact
-#korganizer
-#plasma-systemmonitor
-#spectacle
-#plasma-pa
-#plasma-disks
-#drkonqi
-#plasma-browser-integration
-#plasma-desktop
-#ssdm-kcm
-#networkmanager-qt
-#breeze-grub
-#hunspell
-#hunspell-en_us
+pacstrap /mnt xorg kwin sddm konsole dolphin kinfocenter kate firefox kget kmail vlc kdeconnect kdiff3 krename kfind filelight htop kjots kontact korganizer plasma-systemmonitor spectacle plasma-pa plasma-disks drkonqi plasma-browser-integration plasma-desktop ssdm-kcm networkmanager-qt breeze-grub hunspell hunspell-en_us
 
 kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 2 --group Applets --group 5 --group Configuration --group General --key launchers preferred://browser,preferred://filemanager,applications:org.kde.konsole.desktop
 kwriteconfig5 --file ../.local/share/dolphin/view_properties/global/.direcotry --group Settings --key HiddenFilesShown --type bool false
