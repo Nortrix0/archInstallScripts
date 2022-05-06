9c3073b62f89b09bc23989c959e6f0b990451d71
Nortrix0 <bdgarth1997@gmail.com>
2022-05-06 13:42:02 -0500

    Trying to default no splashscreen

--- kde-encrypted-btrfs.sh
+++ kde-encrypted-btrfs.sh
@@ -2,3 +2,4 @@ clear
 cd "${0%/*}"
 . ./base-encrypted-btrfs.sh |& tee kde-encrypted-btrfs.log
 . ./kde-install.sh |& tee -a kde-encrypted-btrfs.log
+#usr/share/plasma/look-and-feel/org.kde.breezedark.desktop/contents/defaults
