44322aced593afa9000b0e9421a830aa9595fb1c
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-03 12:31:24 -0600

    Update grub.sh
    Added more GRUB cmds from base

--- grub.sh
+++ grub.sh
@@ -1,4 +1,7 @@
+pacstrap /mnt --needed grub grub-btrfs
 arch-chroot /mnt /bin/bash -e <<EOF
 	grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 	grub-mkconfig -o /boot/grub/grub.cfg
 EOF
+echo "system enable ""grub-btrfs.path"" --root=/mnt"
+systemctl enable "grub-btrfs.path" --root=/mnt
