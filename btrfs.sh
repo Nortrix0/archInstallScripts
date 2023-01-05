7d10a70407c66e3f96f882b7a4e9396f1631ddd9
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-05 15:35:37 -0600

    Update btrfs.sh
    Changed local variables to lowercase

--- btrfs.sh
+++ btrfs.sh
@@ -1,7 +1,7 @@
-BTRFS="/dev/disk/by-partlabel/ROOT"
+btrfs="/dev/disk/by-partlabel/ROOT"
 #Format ROOT as BTRFS
-mkfs.btrfs -f $BTRFS                #Makes Conatiner BTRFS
-mount $BTRFS /mnt                   #Mounts BTRFS
+mkfs.btrfs -f $btrfs                #Makes Conatiner BTRFS
+mount $btrfs /mnt                   #Mounts BTRFS
 #Create BTRFS subvolumes
 for volume in @ @home @snapshots @var_log @var_pkgs
 do
@@ -9,12 +9,12 @@ do
 done
 #Mount only new subvolumes
 umount /mnt
-mount -o noatime,discard=async,subvol=@ $BTRFS /mnt
+mount -o noatime,discard=async,subvol=@ $btrfs /mnt
 mkdir -p /mnt/{home,.snapshots,/var/log,/var/cache/pacman/pkg}
-mount -o noatime,discard=async,subvol=@home $BTRFS /mnt/home
-mount -o noatime,discard=async,subvol=@snapshots $BTRFS /mnt/.snapshots
-mount -o noatime,discard=async,subvol=@var_log $BTRFS /mnt/var/log
-mount -o noatime,discard=async,subvol=@var_pkgs $BTRFS /mnt/var/cache/pacman/pkg
+mount -o noatime,discard=async,subvol=@home $btrfs /mnt/home
+mount -o noatime,discard=async,subvol=@snapshots $btrfs /mnt/.snapshots
+mount -o noatime,discard=async,subvol=@var_log $btrfs /mnt/var/log
+mount -o noatime,discard=async,subvol=@var_pkgs $btrfs /mnt/var/cache/pacman/pkg
 #Install BTRFS related Packages
 pacstrap /mnt btrfs-progs rsync snapper snap-pac
 arch-chroot /mnt /bin/bash -e <<EOF
