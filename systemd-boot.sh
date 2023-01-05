9fe862de4b6d94244a8c0340c18f4e6021d22d8f
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-05 15:36:55 -0600

    Update systemd-boot.sh
    Removed DISK Variable and changed local variable to lowercase

--- systemd-boot.sh
+++ systemd-boot.sh
@@ -1,5 +1,4 @@
-DISK=$(lsblk -no NAME /dev/disk/by-partlabel/ROOT)
-BTRFSPARTUUID=$(blkid -s PARTUUID -o value /dev/"$DISK")
+partuuid=$(blkid -s PARTUUID -o value /dev/"$DISK")
 arch-chroot /mnt /bin/bash -e <<EOF
 	bootctl --path=/boot install
 	echo '
@@ -7,5 +6,5 @@ title	Arch Linux
 linux	/vmlinuz-linux
 initrd	/intel-ucode.img
 initrd	/initramfs-linux.img
-options	root="PARTUUID=$BTRFSPARTUUID" rw rootflags=subvol=/@'>> /boot/loader/entries/arch.conf
+options	root="PARTUUID=$partuuid" rw rootflags=subvol=/@'>> /boot/loader/entries/arch.conf
 EOF
