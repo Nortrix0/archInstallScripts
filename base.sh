204a143d8e1796eb4aafed5dd51a4207696c61b4
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-05 15:34:36 -0600

    Update base.sh
    Removed Config Options and changed local variables to lowercase

--- base.sh
+++ base.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env -S bash -e
-set -xv
-#Vars
-read -e -p "Enter the disk to install onto : " -i "/dev/sda" DISK
-read -e -p "Enter the Hostname to use : " -i "AutoArch" HOSTNAME
-read -r -sp "Enter the password for root [password]: " rpass
-ROOTPASS=${rpass:-"password"}
-read -e -p "Enter your username [user]: " -i "user" NEWUSERNAME
-read -r -sp "Enter password for $NEWUSERNAME [password]: " upass
-USERPASS=${upass:-"password"}
-read -e -p "Enter the desired kernel: " -i "linux" KERNEL
-read -e -p "Enter the desired file system [ext4/btrfs]: " -i "ext4" FILESYSTEM
 #Clear Screen
 clear
-#Obtain latest Keyring
-pacman -Sy archlinux-keyring --noconfirm
 #Delete Old partition scheme
 wipefs -af $DISK    #Force Wipe All on Disk
 sgdisk -Zo $DISK    #Destroys existing GPT/MBR structures and clears out all partition data
@@ -23,15 +10,15 @@ sgdisk -Zo $DISK    #Destroys existing GPT/MBR structures and clears out all par
 parted -s $DISK mklabel gpt mkpart ESP fat32 1MiB 513MiB set 1 esp on mkpart ROOT 513MiB 100%
 partprobe "$DISK"                   #Inform Kernel of changes
 sleep 1
-ESP="/dev/disk/by-partlabel/ESP"
+esp="/dev/disk/by-partlabel/ESP"
 #Format ESP as FAT32
-mkfs.fat -F 32 $ESP
-./"$FILESYSTEM".sh
+mkfs.fat -F 32 $esp
+. ./"$FILESYSTEM".sh
 mkdir /mnt/boot
-mount $ESP /mnt/boot                #Mounts ESP
+mount $esp /mnt/boot                #Mounts ESP
 #Install Microcode
-CPU=$(grep vendor_id /proc/cpuinfo)
-if [[ $CPU == *"AuthenticAMD"* ]]; then
+cpu=$(grep vendor_id /proc/cpuinfo)
+if [[ $cpu == *"AuthenticAMD"* ]]; then
     microcode="amd-ucode"
 else
     microcode="intel-ucode"
@@ -53,9 +40,9 @@ arch-chroot /mnt /bin/bash -e <<EOF
     locale-gen
     mkinitcpio -P
     echo "root:$ROOTPASS" | chpasswd
-    useradd -m -G wheel -s /bin/bash "$NEWUSERNAME"
+    useradd -m -G wheel -s /bin/bash "$USER"
     sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-    echo "$NEWUSERNAME:$USERPASS" | chpasswd
+    echo "$USER:$USERPASS" | chpasswd
 EOF
 #Pacman Color and ParallelDownloads
 sed -i 's/#Color/Color/;s/^#ParallelDownloads.*$/ParallelDownloads = 10/' /mnt/etc/pacman.conf
