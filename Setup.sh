7c8f5cf8a5cbbca0eb505b12716e754474c72c2e
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-05 16:47:53 -0600

    Update Setup.sh
    Fixed issues with checks

--- Setup.sh
+++ Setup.sh
@@ -14,17 +14,17 @@ done
 USERPASS=$(dialog --nocancel --passwordbox "Enter Pasword for $USER" 0 0 3>&1 1>&2 2>&3 3>&-)
 FILESYS=$(dialog --nocancel --radiolist "Select Filesystem" 0 0 0 ext4 "" on btrfs "" off 3>&1 1>&2 2>&3 3>&-)
 BOOTLOADER=$(dialog --nocancel --radiolist "Select Bootloader" 0 0 0 systemd-boot "" on grub "" off 3>&1 1>&2 2>&3 3>&-)
-GRAPHICAL=$(dialog --defaultno --yesno "Do You Want Console Only?" 0 0 3>&1 1>&2 2>&3 3>&-)
-if [ $GRAPHICAL == "1"]; then
+GRAPHICAL=$($(dialog --defaultno --yesno "Do You Want Console Only?" 0 0 3>&1 1>&2 2>&3 3>&-) && echo 0 || echo 1)
+if [[ $GRAPHICAL -eq "1" ]]; then
 	CONFIGS=$(dialog --yesno "Do You Want Customized KDE Configs?" 0 0 3>&1 1>&2 2>&3 3>&-)
 fi
 . ./base.sh
 . ./"$BOOTLOADER".sh
-if [ $GRAPHICAL == "1"]; then
+if [[ $GRAPHICAL -eq "1" ]]; then
 	. ./kde-install.sh
 fi
-ADVANCED=$(dialog --no-label "Manually Edit" --yes-label "Reboot" --yesno "What would you like to do?" 0 0 3>&1 1>&2 2>&3 3>&-)
-if [ $ADVANCED == "0"]; then
+ADVANCED=$($(dialog --no-label "Manually Edit" --yes-label "Reboot" --yesno "What would you like to do?" 0 0 3>&1 1>&2 2>&3 3>&-) && echo 1 || echo 0)
+if [[ $ADVANCED -eq "0" ]]; then
 	reboot
 else
 	clear
