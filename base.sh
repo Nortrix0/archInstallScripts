cfb41a30cbe6f066fa0cd25e59ff0d43cbb12db8
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-06 12:26:14 -0600

    Attempt to get rid of most of arch-chroot commands
    Attempt to get rid of most of arch-chroot commands

--- base.sh
+++ base.sh
@@ -6,25 +6,21 @@ else
 fi
 #Install base system
 pacstrap /mnt --needed base $KERNEL $microcode vim nano dhcpcd sudo
-echo "$HOSTNAME" > /mnt/etc/hostname
+echo $HOSTNAME > /mnt/etc/hostname
 #Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 #Setup Locale
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 #Config mkinitcpio
-echo 'HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems)' >> /mnt/etc/mkinitcpio.conf
+#echo 'HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems)' >> /mnt/etc/mkinitcpio.conf
 #Configure System
-arch-chroot /mnt /bin/bash -e <<EOF
-    ln -sf /usr/share/zoneinfo/$(curl -s http://ip-api.com/line?fields=timezone) /etc/localtime
-    hwclock --systohc
-    locale-gen
-    mkinitcpio -P
-    echo "root:$ROOTPASS" | chpasswd
-    useradd -m -G wheel -s /bin/bash "$USER"
-    sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-    echo "$USER:$USERPASS" | chpasswd
-EOF
+ln -sfr /mnt/usr/share/zoneinfo/$(curl -s http://ip-api.com/line?fields=timezone) /mnt/etc/localtime
+arch-chroot /mnt hwclock --systohc
+arch-chroot /mnt locale-gen
+echo "root:$ROOTPASS" | chpasswd -R /mnt
+useradd -mG wheel -p $USERPASS -R /mnt "$USER"
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /mnt/etc/sudoers
 #Pacman Color and ParallelDownloads
 sed -i 's/#Color/Color/;s/^#ParallelDownloads.*$/ParallelDownloads = 10/' /mnt/etc/pacman.conf
 systemctl enable dhcpcd --root=/mnt
