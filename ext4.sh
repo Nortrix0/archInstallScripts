f754c48f51457e5f7c266c7a1c9656f63cc49f23
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-06 15:20:12 -0600

    Added Mkdir and Mount calls

--- ext4.sh
+++ ext4.sh
@@ -1,2 +1,4 @@
 mkfs.ext4 -F "/dev/disk/by-partlabel/ROOT"             #Makes Conatiner EXT4
 mount "/dev/disk/by-partlabel/ROOT" /mnt               #Mounts EXT4
+mkdir /mnt/boot
+mount $ESP /mnt/boot                                   #Mounts ESP
