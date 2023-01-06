e4c5f078d69426e59142aff7d8e9a22b12895e28
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-06 08:55:14 -0600

    Update Setup.sh
    Added missing section on CONFIGS dialog

--- Setup.sh
+++ Setup.sh
@@ -1,11 +1,14 @@
 clear
-#Set ParallelDownloads on archIso to help speed up install
+#Set ParallelDownloads on archIso to help speed up install [a-zA-Z][a-zA-Z0-9-]{0,62}
 sed -i 's|^#ParallelDownloads.*$|ParallelDownloads = 10|' /etc/pacman.conf
 #Obtain latest Keyring
 pacman -Sy dialog --noconfirm
 DISK=$(dialog --nocancel --menu "Select Disk" 0 0 5 $(lsblk -rnpSo NAME,SIZE) 3>&1 1>&2 2>&3 3>&-)
 KERNEL=$(dialog --nocancel --radiolist "Select Kernel" 0 0 2 linux Stable on linux-hardened Hardened off linux-lts Longterm off linux-zen Zen off 3>&1 1>&2 2>&3 3>&-)
 HOSTNAME=$(dialog --nocancel --inputbox "Enter Hostname" 0 0 "ArchAuto" 3>&1 1>&2 2>&3 3>&-)
+while ! [[ $USER =~ ^[a-z_][a-z0-9_-]{0,30}[$]?$ ]]; do
+	HOSTNAME=$(dialog --nocancel --inputbox "$HOSTNAME Invalid Must Be At Most 63 Characters And Only Contain A-Z and - but can not start with -" 0 0 "ArchAuto" 3>&1 1>&2 2>&3 3>&-)
+done
 ROOTPASS=$(dialog --nocancel --passwordbox "Enter Pasword for Root" 0 0 3>&1 1>&2 2>&3 3>&-)
 USER=$(dialog --nocancel --inputbox "Enter Username" 0 0 "user" 3>&1 1>&2 2>&3 3>&-)
 while ! [[ $USER =~ ^[a-z_][a-z0-9_-]{0,30}[$]?$ ]]; do
@@ -13,10 +16,13 @@ while ! [[ $USER =~ ^[a-z_][a-z0-9_-]{0,30}[$]?$ ]]; do
 done
 USERPASS=$(dialog --nocancel --passwordbox "Enter Pasword for $USER" 0 0 3>&1 1>&2 2>&3 3>&-)
 FILESYS=$(dialog --nocancel --radiolist "Select Filesystem" 0 0 0 ext4 "" on btrfs "" off 3>&1 1>&2 2>&3 3>&-)
+if [[ $FILESYS -eq "btrfs" ]]; then
+	ENCRYPTPASS=$(dialog --nocancel --passwordbox "Enter Password for Encryption, Leave Blank If You Do Not Want Encryption" 0 0 3>&1 1>&2 2>&3 3>&-)
+fi
 BOOTLOADER=$(dialog --nocancel --radiolist "Select Bootloader" 0 0 0 systemd-boot "" on grub "" off 3>&1 1>&2 2>&3 3>&-)
 GRAPHICAL=$($(dialog --defaultno --yesno "Do You Want Console Only?" 0 0 3>&1 1>&2 2>&3 3>&-) && echo 0 || echo 1)
 if [[ $GRAPHICAL -eq "1" ]]; then
-	CONFIGS=$(dialog --yesno "Do You Want Customized KDE Configs?" 0 0 3>&1 1>&2 2>&3 3>&-)
+	CONFIGS=$($(dialog --yesno "Do You Want Customized KDE Configs?" 0 0 3>&1 1>&2 2>&3 3>&-) && echo 0 || echo 1)
 fi
 . ./base.sh
 . ./"$BOOTLOADER".sh
