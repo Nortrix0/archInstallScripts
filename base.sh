ccf23290c8ba1f531f82da4c8d331a686f3bc2ce
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-06 08:29:32 -0600

    Update base.sh
    Removed Format Section and bash header

--- base.sh
+++ base.sh
@@ -1,24 +1,5 @@
-#!/usr/bin/env -S bash -e
-#Clear Screen
-clear
-#Delete Old partition scheme
-wipefs -af $DISK    #Force Wipe All on Disk
-sgdisk -Zo $DISK    #Destroys existing GPT/MBR structures and clears out all partition data
-#Create new partition scheme
-#Doesn't ask for input from user, Creates New disklabel of type GPT, Create new partition Labeled ESP of type fat32 and is 512 MiB in size
-#, Sets partition as bootable, Create new partition Labeled ROOT that uses the rest of the drive space
-parted -s $DISK mklabel gpt mkpart ESP fat32 1MiB 513MiB set 1 esp on mkpart ROOT 513MiB 100%
-partprobe "$DISK"                   #Inform Kernel of changes
-sleep 1
-esp="/dev/disk/by-partlabel/ESP"
-#Format ESP as FAT32
-mkfs.fat -F 32 $esp
-. ./"$FILESYS".sh
-mkdir /mnt/boot
-mount $esp /mnt/boot                #Mounts ESP
-#Install Microcode
-cpu=$(grep vendor_id /proc/cpuinfo)
-if [[ $cpu == *"AuthenticAMD"* ]]; then
+#Determine Microcode
+if [[ $(grep vendor_id /proc/cpuinfo) == *"AuthenticAMD"* ]]; then
     microcode="amd-ucode"
 else
     microcode="intel-ucode"
