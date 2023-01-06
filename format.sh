df84255041f15d06f83c76af2a9d51db05c4a353
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-06 08:30:01 -0600

    Create format.sh
    Transferred format section from base.sh

---
+++ format.sh
@@ -0,0 +1,15 @@
+#Delete Old partition scheme
+wipefs -af $DISK    #Force Wipe All on Disk
+sgdisk -Zo $DISK    #Destroys existing GPT/MBR structures and clears out all partition data
+#Create new partition scheme
+#Doesn't ask for input from user, Creates New disklabel of type GPT, Create new partition Labeled ESP of type fat32 and is 512 MiB in size
+#, Sets partition as bootable, Create new partition Labeled ROOT that uses the rest of the drive space
+parted -s $DISK mklabel gpt mkpart ESP fat32 1MiB 513MiB set 1 esp on mkpart ROOT 513MiB 100%
+partprobe "$DISK"                   #Inform Kernel of changes
+sleep 1
+esp="/dev/disk/by-partlabel/ESP"
+#Format ESP as FAT32
+mkfs.fat -F 32 $esp
+. ./"$FILESYS".sh
+mkdir /mnt/boot
+mount $esp /mnt/boot                #Mounts ESP
