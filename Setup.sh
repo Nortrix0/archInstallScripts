ce5d915d4a3f363a9e20ad2ce50bf79c6d34d576
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-09 09:57:29 -0600

    Changed comments

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,6 @@
 clear
-#Set ParallelDownloads on archIso to help speed up install [a-zA-Z][a-zA-Z0-9-]{0,62}
+#Set ParallelDownloads on ArchIso to help speed up install
 sed -i 's|^#ParallelDownloads.*$|ParallelDownloads = 10|' /etc/pacman.conf
-#Obtain latest Keyring
 pacman -Sy dialog --noconfirm
 DISK=$(dialog --nocancel --menu "Select Disk" 0 0 5 $(lsblk -rnpSo NAME,SIZE) 3>&1 1>&2 2>&3 3>&-)
 KERNEL=$(dialog --nocancel --radiolist "Select Kernel" 0 0 2 linux Stable on linux-hardened Hardened off linux-lts Longterm off linux-zen Zen off 3>&1 1>&2 2>&3 3>&-)
