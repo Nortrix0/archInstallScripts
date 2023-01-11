aa16a8a166c6d300835e709340515526e60a2340
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-11 11:25:20 -0600

    Fixed system service

--- grub.sh
+++ grub.sh
@@ -3,5 +3,4 @@ arch-chroot /mnt /bin/bash -e <<EOF
 	grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 	grub-mkconfig -o /boot/grub/grub.cfg
 EOF
-echo "system enable ""grub-btrfs.path"" --root=/mnt"
-systemctl enable "grub-btrfs.path" --root=/mnt
+systemctl enable "grub-btrfsd" --root=/mnt
