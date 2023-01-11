a7c5ec59a2cdc72532b56eac024abf2b1b654b2a
Nortrix0 <minecrafternortrix@gmail.com>
2023-01-11 15:16:38 -0600

    Added logic for if ECRYPTPASS is set

--- grub/install.sh
+++ grub/install.sh
@@ -1,2 +1,6 @@
+if [[ $ENCRYPTPASS != "" ]]; then
+  UUID=$(blkid -s UUID -o value "/dev/disk/by-partlabel/CRYPTROOT")
+  sed -i "\,^GRUB_CMDLINE_LINUX=\"\",s,\",&rd.luks.name=$UUID=cryptroot root=$ROOT," /mnt/etc/default/grub
+fi
 arch-chroot /mnt /bin/bash grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 arch-chroot /mnt /bin/bash grub-mkconfig -o /boot/grub/grub.cfg
