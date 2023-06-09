ccf4b8546f6385dd42729fdca9b972df237ca004
Nortrix0 <minecrafternortrix@gmail.com>
2023-06-09 12:25:00 -0500

    Update base.sh
    Added BTRFS Logic

--- Base/base.sh
+++ Base/base.sh
@@ -11,6 +11,24 @@ ESP="/dev/disk/by-partlabel/ESP"
 #Format ESP as FAT32
 mkfs.fat -F 32 $ESP
 ROOT="/dev/disk/by-partlabel/ROOT"
+#Format ROOT as BTRFS
+mkfs.btrfs -f $ROOT                #Makes Conatiner BTRFS
+mount $ROOT /mnt                   #Mounts BTRFS
+#Create BTRFS subvolumes
+for volume in @ @home @snapshots @var_log @var_pkgs
+do
+    btrfs subvolume create /mnt/$volume
+done
+#Mount only new subvolumes
+umount /mnt
+mount -o noatime,discard=async,subvol=@ $ROOT /mnt
+mkdir -p /mnt/{home,.snapshots,/var/log,/var/cache/pacman/pkg}
+mount -o noatime,discard=async,subvol=@home $ROOT /mnt/home
+mount -o noatime,discard=async,subvol=@snapshots $ROOT /mnt/.snapshots
+mount -o noatime,discard=async,subvol=@var_log $ROOT /mnt/var/log
+mount -o noatime,discard=async,subvol=@var_pkgs $ROOT /mnt/var/cache/pacman/pkg
+mkdir /mnt/boot
+mount $ESP /mnt/boot                #Mounts ESP
 #Determine Microcode
 microcode=$([[ $(grep vendor_id /proc/cpuinfo) == *"AuthenticAMD"* ]] && echo "amd-ucode" || echo "intel-ucode")
 sed -i "s|microcode|$microcode|" ./Base/packages.txt
