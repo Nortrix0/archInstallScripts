33691395961d94dbdd4e9be8faa02197457699ea
Nortrix0 <minecrafternortrix@gmail.com>
2023-06-13 12:00:57 -0500

    Update base.sh
    Backup /usr

--- Base/base.sh
+++ Base/base.sh
@@ -15,15 +15,16 @@ ROOT="/dev/disk/by-partlabel/ROOT"
 mkfs.btrfs -f $ROOT                #Makes Conatiner BTRFS
 mount $ROOT /mnt                   #Mounts BTRFS
 #Create BTRFS subvolumes
-for volume in @ @home @var_log @var_pkgs
+for volume in @ @home @usr @var_log @var_pkgs
 do
     btrfs subvolume create /mnt/$volume
 done
 #Mount only new subvolumes
 umount /mnt
 mount -o noatime,discard=async,subvol=@ $ROOT /mnt
-mkdir -p /mnt/{home,/var/log,/var/cache/pacman/pkg}
+mkdir -p /mnt/{home, usr,/var/log,/var/cache/pacman/pkg}
 mount -o noatime,discard=async,subvol=@home $ROOT /mnt/home
+mount -o noatime,discard=async,subvol=@home $ROOT /mnt/usr
 mount -o noatime,discard=async,subvol=@var_log $ROOT /mnt/var/log
 mount -o noatime,discard=async,subvol=@var_pkgs $ROOT /mnt/var/cache/pacman/pkg
 mkdir /mnt/boot
