80658a5ec24d6cc942f380301da18b8202fe8a71
Nortrix0 <minecrafternortrix@gmail.com>
2023-06-13 10:21:11 -0500

    Update base.sh
    Remove If Statement

--- Base/base.sh
+++ Base/base.sh
@@ -33,12 +33,9 @@ microcode=$([[ $(grep vendor_id /proc/cpuinfo) == *"AuthenticAMD"* ]] && echo "a
 sed -i "s|microcode|$microcode|" ./Base/packages.txt
 #Install base system
 
-RESULT=$?
 until pacstrap /mnt --needed - < ./Base/packages.txt; do
-	if [ $? -ne 0 ]; then
-		echo "Failed Getting Packages, will try again in 5 Seconds.  Press Control+C to cancel"
-		sleep 5
-	fi
+	echo "Failed Getting Packages, will try again in 5 Seconds.  Press Control+C to cancel"
+	sleep 5
 done
 echo $HOSTNAME > /mnt/etc/hostname
 #Generate fstab
