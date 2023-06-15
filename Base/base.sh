2ae33fe23f97da00ae927f0e782922dc7cef1b58
Nortrix0 <minecrafternortrix@gmail.com>
2023-06-15 11:31:42 -0500

    Update base.sh
    Change HOOKS

--- Base/base.sh
+++ Base/base.sh
@@ -4,7 +4,7 @@ sgdisk -Zo $DISK    #Destroys existing GPT/MBR structures and clears out all par
 #Create new partition scheme
 #Doesn't ask for input from user, Creates New disklabel of type GPT, Create new partition Labeled ESP of type fat32 and is 512 MiB in size
 #, Sets partition as bootable, Create new partition Labeled ROOT that uses the rest of the drive space
-parted -s $DISK mklabel gpt mkpart ESP fat32 1MiB 513MiB set 1 esp on mkpart ROOT 513MiB 100%
+parted -s $DISK mklabel gpt mkpart ESP fat32 0% 513MiB set 1 esp on mkpart ROOT 513MiB 100%
 partprobe "$DISK"                   #Inform Kernel of changes
 sleep 1
 ESP="/dev/disk/by-partlabel/ESP"
@@ -15,10 +15,9 @@ ROOT="/dev/disk/by-partlabel/ROOT"
 mkfs.btrfs -f $ROOT                #Makes Conatiner BTRFS
 mount $ROOT /mnt                   #Mounts BTRFS
 #Create BTRFS subvolumes
-for volume in @ @home @var_log
-do
-    btrfs subvolume create /mnt/$volume
-done
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@var_log
 #Mount only new subvolumes
 umount /mnt
 mount -o noatime,discard=async,subvol=@ $ROOT /mnt
@@ -34,7 +33,6 @@ mount $ESP /mnt/boot                #Mounts ESP
 microcode=$([[ $(grep vendor_id /proc/cpuinfo) == *"AuthenticAMD"* ]] && echo "amd-ucode" || echo "intel-ucode")
 sed -i "s|microcode|$microcode|" ./Base/packages.txt
 #Install base system
-
 until pacstrap /mnt --needed - < ./Base/packages.txt; do
 	echo "Failed Getting Packages, will try again in 5 Seconds.  Press Control+C to cancel"
 	sleep 5
@@ -46,7 +44,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 #Config mkinitcpio
-sed -i 's/^HOOKS=.*$/HOOKS=(base systemd autodetect keyboard modconf block sd-encrypt filesystems grub-btrfs-overlayfs)/' >> /mnt/etc/mkinitcpio.conf
+sed -i 's/^HOOKS=.*$/HOOKS=(base systemd autodetect modconf kms block keyboard sd-vconsole lvm2 filesystems fsck grub-btrfs-overlayfs)/' >> /mnt/etc/mkinitcpio.conf
 #Configure System
 ln -sfr /mnt/usr/share/zoneinfo/America/Chicago /mnt/etc/localtime
 arch-chroot /mnt hwclock --systohc
@@ -59,9 +57,7 @@ sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /mnt/etc/sudoers
 sed -i 's/#Color/Color/;s/^#ParallelDownloads.*$/ParallelDownloads = 10/' /mnt/etc/pacman.conf
 arch-chroot /mnt snapper --no-dbus -c root create-config /
 arch-chroot /mnt chown :wheel /.snapshots
-sed -i 's/ALLOW_USERS=""/ALLOW_USERS="'"$USER"'"/' /mnt/etc/snapper/configs/root
-sed -i 's/TIMELINE_LIMIT_MONTHLY="10"/TIMELINE_LIMIT_MONTHLY="0"/' /mnt/etc/snapper/configs/root
-sed -i 's/TIMELINE_LIMIT_YEARLY="10"/TIMELINE_LIMIT_YEARLY="0"/' /mnt/etc/snapper/configs/root
+sed -i 's/ALLOW_USERS=.*$/ALLOW_USERS="'"$USER"'"/; s/MONTHLY=.*$/MONTHLY="0"/; s/YEARLY=.*$/YEARLY="0"/' /mnt/etc/snapper/configs/root
 while read s; do
 	systemctl enable $s --root=/mnt
 done <./Base/services.txt
