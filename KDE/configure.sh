e823297140cec9a527ff5936214693bfa89b1bec
Nortrix0 <minecrafternortrix@gmail.com>
2023-12-18 12:06:21 -0600

    Update configure.sh
    Changed sed command

--- KDE/configure.sh
+++ KDE/configure.sh
@@ -3,6 +3,6 @@ cp -r ./KDE/KDE_Local_dotfiles /mnt/home/$USER/.local
 #khotkeysrc kglobalshortcutsrc
 
 arch-chroot /mnt chown -R "$USER" /home/$USER/.config /home/$USER/.local
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /mnt/etc/pacman.conf
+sed -i -z 's|#\[multilib]\n#|[multilib]\n|' /mnt/etc/pacman.conf
 sed -i 's/Numlock=.*/Numlock=on/' /mnt/usr/lib/sddm/sddm.conf.d/default.conf
 rm /mnt/usr/share/applications/{assistant.desktop,avahi-discover.desktop,bssh.desktop,bvnc.desktop,designer.desktop,linguist.desktop,org.kde.kuserfeedback-console.desktop,org.kde.plasma.emojier.desktop,qdbusviewer.desktop,qv4l2.desktop,qvidcap.desktop}
