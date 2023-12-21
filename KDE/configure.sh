ec0b076295246a707f93b966d22a7b3f90c4eedc
Nortrix0 <minecrafternortrix@gmail.com>
2023-12-21 14:45:12 -0600

    Update configure.sh

--- KDE/configure.sh
+++ KDE/configure.sh
@@ -1,16 +1,8 @@
-#cp -r ./KDE/KDE_Config_dotfiles /mnt/home/$USER/.config
+cp -r ./KDE/KDE_Config_dotfiles /mnt/home/$USER/.config
 cp -r ./KDE/KDE_Local_dotfiles /mnt/home/$USER/.local
 #khotkeysrc kglobalshortcutsrc
 
 arch-chroot /mnt chown -R "$USER" /home/$USER/.config /home/$USER/.local
-arch-chroot -u "$USER" /mnt lookandfeeltool -a org.kde.breezedark.desktop
-arch-chroot -u "$USER" /mnt kwriteconfig5 --file ksplashrc --group KSplash --key Engine none
-arch-chroot -u "$USER" /mnt kwriteconfig5 --file ksmserverrc --group General --key confirmLogout false
-arch-chroot -u "$USER" /mnt kwriteconfig5 --file kscreenlockerrc --group Daemon --key Timeout 15
-arch-chroot -u "$USER" /mnt kwriteconfig5 --file dolphinrc --group General --key ShowFullPath true
-arch-chroot -u "$USER" /mnt kwriteconfig5 --file katerc --group KTextEditor Document --key On-The-Fly Spellcheck true
-arch-chroot -u "$USER" /mnt kwriteconfig5 --file katerc --group KTextEditor View --key Show Line Count true
-arch-chroot -u "$USER" /mnt kwriteconfig5 --file katerc --group KTextEditor View --key Show Word Count true
 sed -i -z 's|#\[multilib]\n#|[multilib]\n|' /mnt/etc/pacman.conf
 sed -i 's/Numlock=.*/Numlock=on/' /mnt/usr/lib/sddm/sddm.conf.d/default.conf
 rm /mnt/usr/share/applications/{assistant.desktop,avahi-discover.desktop,bssh.desktop,bvnc.desktop,designer.desktop,linguist.desktop,org.kde.kuserfeedback-console.desktop,org.kde.plasma.emojier.desktop,qdbusviewer.desktop,qv4l2.desktop,qvidcap.desktop}
