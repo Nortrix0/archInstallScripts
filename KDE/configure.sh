28ea6e1a0c7fc4960790140ab5d6bc3fa218a553
Nortrix0 <minecrafternortrix@gmail.com>
2023-12-22 08:06:23 -0600

    Update configure.sh

--- KDE/configure.sh
+++ KDE/configure.sh
@@ -1,8 +1,7 @@
-cp -r ./KDE/KDE_Config_dotfiles /mnt/home/$USER/.config
-cp -r ./KDE/KDE_Local_dotfiles /mnt/home/$USER/.local
+install -o "$USER" -t ./KDE/KDE_Config_dotfiles /mnt/home/$USER/.config
+install -o "$USER" ./KDE/KDE_Local_dotfiles/* /mnt/home/$USER/.local
 #khotkeysrc kglobalshortcutsrc
 
-arch-chroot /mnt chown -R "$USER" /home/$USER/.config /home/$USER/.local
 sed -i -z 's|#\[multilib]\n#|[multilib]\n|' /mnt/etc/pacman.conf
 sed -i 's/Numlock=.*/Numlock=on/' /mnt/usr/lib/sddm/sddm.conf.d/default.conf
 rm /mnt/usr/share/applications/{assistant.desktop,avahi-discover.desktop,bssh.desktop,bvnc.desktop,designer.desktop,linguist.desktop,org.kde.kuserfeedback-console.desktop,org.kde.plasma.emojier.desktop,qdbusviewer.desktop,qv4l2.desktop,qvidcap.desktop}
