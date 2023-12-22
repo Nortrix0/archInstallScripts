c4cb90ac28f3453240074338a69c552c35dc8af4
Nortrix0 <minecrafternortrix@gmail.com>
2023-12-22 09:14:51 -0600

    Update Setup.sh
    Change some menus

--- Setup.sh
+++ Setup.sh
@@ -9,7 +9,7 @@ pacman -Sy dialog --noconfirm
 #mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 #reflector --latest 20 --protocol https --sort rate --country 'United States' --save /etc/pacman.d/mirrorlist
 DISK=$(dialog --nocancel --menu "Select Disk" 0 0 5 $(lsblk -rnpSo NAME,SIZE) 3>&1 1>&2 2>&3 3>&-)
-#kernel=$(dialog --nocancel --radiolist "Select Kernel" 0 0 2 linux Stable on linux-hardened Hardened off linux-lts Longterm off 3>&1 1>&2 2>&3 3>&-)
+#kernel=$(dialog --nocancel --menu "Select Kernel" 0 0 2 linux Stable linux-hardened Hardened linux-lts Longterm 3>&1 1>&2 2>&3 3>&-)
 kernel=linux
 cp ./Base/packages.txt ./install_packages.txt
 cp ./Base/services.txt ./install_services.txt
@@ -26,19 +26,19 @@ while ! [[ $USER =~ ^[a-z_][a-z0-9_-]{0,30}[$]?$ ]]; do
 	USER=$(dialog --nocancel --inputbox "$USER Invalid Must Be At Most 32 Characters And lowercase" 0 0 $(echo "$USER" | tr '[:upper:]' '[:lower:]') 3>&1 1>&2 2>&3 3>&-)
 done
 USERPASS=$(dialog --nocancel --passwordbox "Enter Password for $USER" 0 0 3>&1 1>&2 2>&3 3>&-)
-USEROOT=$(dialog --nocancel --radiolist "How do you want ROOT's password?" 0 0 0 "Same As User" "" on "New Password" "" off "Disabled" "" off 3>&1 1>&2 2>&3 3>&-)
+USEROOT=$(dialog --nocancel --menu "How do you want ROOT's password?" 0 0 0 "Same As User" "" "New Password" "" "Disabled" "" 3>&1 1>&2 2>&3 3>&-)
 if [[ $USEROOT == "Same As User" ]]; then
 	ROOTPASS=$USERPASS
 else if [[ $USEROOT == "New Password"]]; then
 	ROOTPASS=$(dialog --nocancel --passwordbox "Enter Pasword for Root" 0 0 3>&1 1>&2 2>&3 3>&-)
 fi
-DESKTOP=$(dialog --nocancel --radiolist "Which Desktop Do You Want?" 0 0 0 KDE "" on KDE6 "" off Console "" off 3>&1 1>&2 2>&3 3>&-)
+DESKTOP=$(dialog --nocancel --menu "Which Desktop Do You Want?" 0 0 0 KDE "" KDE6 "" Console "" 3>&1 1>&2 2>&3 3>&-)
 if [[ $DESKTOP == "KDE" ]]; then
 	cat ./KDE/packages.txt >> ./install_packages.txt
 	cat ./KDE/services.txt >> ./install_services.txt
 	CONFIGS=$($(dialog --yesno "Do You Want Customized KDE Configs?" 0 0 3>&1 1>&2 2>&3 3>&-) && echo "Yes" || echo "No")
 	if [[ systemd-detect-virt == "none" ]]; then
-		GRAPHICS=$(dialog --nocancel --radiolist "Which Graphics Driver Do You Want" 0 0 0 AMD "" on Intel "" off NVIDIA "" off  3>&1 1>&2 2>&3 3>&-)
+		GRAPHICS=$(dialog --nocancel --menu "Which Graphics Driver Do You Want" 0 0 0 AMD "" Intel "" NVIDIA "" 3>&1 1>&2 2>&3 3>&-)
 		if [[ $GRAPHICS == "AMD" ]]; then
 			echo -e "lib32-vulkan-radeon\n" >> ./install_packages.txt
 		fi
@@ -58,6 +58,7 @@ else
 		echo -e "plasma-meta" >> ./install_packages.txt
 	fi
 fi
+USEADVANCED=$(dialog --nocancel --menu "Do you want to reboot when install is done?" 0 0 0 "Yes" "" "Ask Me After Install" "" 3>&1 1>&2 2>&3 3>&-)
 . ./Base/base.sh
 if [[ $CONFIGS == "Yes" ]]; then
 	. ./KDE/configure.sh
@@ -65,7 +66,11 @@ fi
 if [[ $DESKTOP == "KDE6" ]]; then
 	echo -e "[kde-unstable]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /mnt/etc/pacman.conf
 fi
-ADVANCED=$(dialog --nocancel --menu "What would you like to do?" 0 0 0 "Reboot" "" "Manually Edit" "" 3>&1 1>&2 2>&3 3>&-)
+if [[ $USERADVANCED == "Ask Me After Install" ]]; then
+	ADVANCED=$(dialog --nocancel --menu "What would you like to do?" 0 0 0 "Reboot" "" "Manually Edit" "" 3>&1 1>&2 2>&3 3>&-)
+else
+	ADVANCED="Reboot"]
+fi
 if [[ $ADVANCED == "Reboot" ]]; then
 	reboot
 else
