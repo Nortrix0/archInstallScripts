1a723ba4b391956a035e77771003326917b1fa9e
Nortrix0 <minecrafternortrix@gmail.com>
2024-01-04 15:04:44 -0600

    Add TODO and fix \n

--- Desktops/KDE/Configs/Bowen's/configure.sh
+++ Desktops/KDE/Configs/Bowen's/configure.sh
@@ -3,5 +3,5 @@
 sed -i -z 's|#\[multilib]\n#|[multilib]\n|' /mnt/etc/pacman.conf
 sed -i 's/Numlock=.*/Numlock=on/' /mnt/usr/lib/sddm/sddm.conf.d/default.conf
 for app in {assistant.desktop,avahi-discover.desktop,bssh.desktop,bvnc.desktop,designer.desktop,linguist.desktop,org.kde.kuserfeedback-console.desktop,org.kde.plasma.emojier.desktop,qdbusviewer.desktop,qv4l2.desktop,qvidcap.desktop}; do
-    echo "\nNoDisplay=true" >> /mnt/usr/share/applications/$app
+    echo "NoDisplay=true" >> /mnt/usr/share/applications/$app
 done
