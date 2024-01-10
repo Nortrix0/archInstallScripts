dd601fb1f1c8e8e892adf94c484aaa6b9e0c8572
Nortrix0 <minecrafternortrix@gmail.com>
2024-01-10 12:04:34 -0600

    Change from a for loop to tee

--- Desktops/KDE/Configs/Bowens/configure.sh
+++ Desktops/KDE/Configs/Bowens/configure.sh
@@ -2,6 +2,4 @@
 
 sed -i -z 's|#\[multilib]\n#|[multilib]\n|' /mnt/etc/pacman.conf
 sed -i 's/Numlock=.*/Numlock=on/' /mnt/usr/lib/sddm/sddm.conf.d/default.conf
-for app in {assistant.desktop,avahi-discover.desktop,bssh.desktop,bvnc.desktop,designer.desktop,linguist.desktop,org.kde.kuserfeedback-console.desktop,org.kde.plasma.emojier.desktop,qdbusviewer.desktop,qv4l2.desktop,qvidcap.desktop}; do
-    echo "NoDisplay=true" >> /mnt/usr/share/applications/$app
-done
+echo "NoDisplay=true" | tee -a /mnt/usr/share/applications/{assistant.desktop,avahi-discover.desktop,bssh.desktop,bvnc.desktop,designer.desktop,linguist.desktop,org.kde.kuserfeedback-console.desktop,org.kde.plasma.emojier.desktop,qdbusviewer.desktop,qv4l2.desktop,qvidcap.desktop}
