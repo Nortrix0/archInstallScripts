344c5e9e959958d9b8ce10e4ffbb6d9afa684e49
Nortrix0 <minecrafternortrix@gmail.com>
2024-01-11 14:27:16 -0600

    Fix password prompts

--- Base/prompts.sh
+++ Base/prompts.sh
@@ -9,12 +9,12 @@ USER=$(whiptail --output-fd 3 --nocancel --inputbox "Enter Username" 0 0 "" 3>&1
 while ! [[ $USER =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]] do
 	USER=$(whiptail --output-fd 3 --nocancel --inputbox "$USER Invalid Must Be At Most 32 Characters And lowercase" 0 0 $(echo "$USER" | tr '[:upper:]' '[:lower:]') 3>&1 1>&2 2>&3)
 done
-USERPASS=$(whiptail --output-fd 3 --nocancel --passwordbox "Enter Password for $USER" 0 0 3>&1 1>&2 2>&3)
+USERPASS=$(whiptail --output-fd 3 --nocancel --passwordbox "Enter Password for $USER" 7 0 3>&1 1>&2 2>&3)
 USEROOT=$(whiptail --output-fd 3 --nocancel --menu "How do you want ROOT's password?" 0 0 0 "Same As User" "" "New Password" "" "Disabled" "" 3>&1 1>&2 2>&3)
 if [[ $USEROOT == "Same As User" ]] then
 	ROOTPASS=$USERPASS
 elif [[ $USEROOT == "New Password" ]] then
-	ROOTPASS=$(whiptail --output-fd 3 --nocancel --passwordbox "Enter Password for Root" 0 0 3>&1 1>&2 2>&3)
+	ROOTPASS=$(whiptail --output-fd 3 --nocancel --passwordbox "Enter Password for Root" 7 0 3>&1 1>&2 2>&3)
 fi
 DESKTOP=$(whiptail --output-fd 3 --nocancel --menu "Which Desktop Do You Want?" 0 0 0 $(find ./Desktops/* -maxdepth 0 -type d  -printf '%f ​ ') 3>&1 1>&2 2>&3)
 CONFIGS=$( [[ ! -d "./Desktops/$DESKTOP/Configs" ]] && echo "None" || echo $(whiptail --output-fd 3 --nocancel --menu "Do You Want Customized $DESKTOP Configs?" 0 0 0 None ​ $(find ./Desktops/$DESKTOP/Configs/* -maxdepth 0 -type d -printf '%f ​ ') 3>&1 1>&2 2>&3))
