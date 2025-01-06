a4acf7b623990a93a7aae69709d04af576ec6001
Nortrix0 <minecrafternortrix@gmail.com>
2025-01-06 10:12:58 -0600

    Tset to get rid of */Desktop searches

--- Base/prompts.sh
+++ Base/prompts.sh
@@ -1,9 +1,10 @@
 REPOLIST=$(find ./Repository/* -type f -exec sh -c 'echo -e $(basename "{}") $(cat "{}") on ' \;)
-REPOS=$(whiptail --nocancel --checklist "Select which Repos you want to use" 0 0 5 $REPOLIST 3>&1 1>&2 2>&3 | sed 's|"||g')
+REPOS=$(whiptail --nocancel --separate-output --checklist "Select which Repos you want to use" 0 0 5 $REPOLIST 3>&1 1>&2 2>&3 | sed 's|"||g')
 for repo in $REPOS; do
-	cat ./Repository/$repo | xargs -I {} git clone "{}"
+	cat ./Repository/$repo | xargs -I {} git clone "{}" $repo
 done
-DISK=$(whiptail --nocancel --menu "Select Disk" 0 0 5 $(lsblk -rnpo NAME,SIZE | grep -E '.*[0-9]{2,}.*G$|.*T$') 3>&1 1>&2 2>&3)
+REPOS=$(echo $REPOS | tr ' ' ',')
+DISK=$(whiptail --nocancel --menu "Select Disk" 0 0 5 $(lsblk -rnpdo NAME,SIZE | grep -E '.*[0-9]{2,}.*G$|.*T$') 3>&1 1>&2 2>&3)
 ENCRYPT=$(whiptail --yesno "Do you want to have the Drive Encrypted?" 0 0 0 3>&1 1>&2 2>&3 && echo true || echo false)
 if $ENCRYPT; then
     ENCRYPTPASS=$(whiptail --nocancel --passwordbox "Enter Encryption Password" 7 0 3>&1 1>&2 2>&3)
@@ -25,13 +26,13 @@ if [[ $USEROOT == "Same As User" ]] then
 elif [[ $USEROOT == "New Password" ]] then
 	ROOTPASS=$(whiptail --nocancel --passwordbox "Enter Password for Root" 7 0 3>&1 1>&2 2>&3)
 fi
-DESKTOP=$(whiptail --nocancel --noitem --menu "Which Desktop Do You Want?" 0 0 0 $(find ./*/Desktops/* -maxdepth 0 -type d  -printf '%f ​ ') 3>&1 1>&2 2>&3)
-CONFIGS=$( [[ ! $( find ./*/Desktops/$DESKTOP/Configs) ]] && echo "None" || echo $(whiptail --nocancel --noitem --menu "Do You Want Customized $DESKTOP Configs?" 0 0 0 None ​ $(find ./*/Desktops/$DESKTOP/Configs/* -maxdepth 0 -type d -printf '%f ​ ') 3>&1 1>&2 2>&3))
+DESKTOP=$(whiptail --nocancel --noitem --menu "Which Desktop Do You Want?" 0 0 0 $(find ./{$REPOS}/Desktops/* -maxdepth 0 -type d  -printf '%f ​ ') 3>&1 1>&2 2>&3)
+CONFIGS=$( [[ ! $( find ./{$REPOS}/Desktops/$DESKTOP/Configs) ]] && echo "None" || echo $(whiptail --nocancel --noitem --menu "Do You Want Customized $DESKTOP Configs?" 0 0 0 None ​ $(find ./*/Desktops/$DESKTOP/Configs/* -maxdepth 0 -type d -printf '%f ​ ') 3>&1 1>&2 2>&3))
 if [[ $CONFIGS != "None" ]] then
 	CHAOTIC=$(whiptail --yesno "Do you want to use Chaotic AUR?" 0 0 0 3>&1 1>&2 2>&3 && echo true || echo false)
 fi
-$(. ./*/Desktops/$DESKTOP/prompts.sh || true) || echo "./*/Desktops/$DESKTOP/prompts.sh NOT FOUND"
-$(. ./*/Desktops/$DESKTOP/Configs/$CONFIGS/prompts.sh || true) || echo "./*/Desktops/$DESKTOP/Configs/$CONFIGS/prompts.sh NOT FOUND"
+$(. ./{$REPOS}/Desktops/$DESKTOP/prompts.sh || true) || echo "./{$REPOS}/Desktops/$DESKTOP/prompts.sh NOT FOUND"
+$(. ./{$REPOS}/Desktops/$DESKTOP/Configs/$CONFIGS/prompts.sh || true) || echo "./{$REPOS}/Desktops/$DESKTOP/Configs/$CONFIGS/prompts.sh NOT FOUND"
 BACKUP=$(whiptail --nocancel --noitem --menu "Which Backup Option do you prefer?" 0 0 0 Snapper ​ Timeshift ​ 3>&1 1>&2 2>&3)
 REBOOT=$(whiptail --nocancel --noitem --menu "Do you want to reboot when install is done?" 0 0 0 Yes ​ No ​ "Stop Install Now" ​ 3>&1 1>&2 2>&3)
 #REBOOT=$(whiptail --yesno "Do you want to reboot when install is done?" 0 0 0 3>&1 1>&2 2>&3 && echo true || echo false)
